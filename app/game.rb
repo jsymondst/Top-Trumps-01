410e419919781c96e4870c1159b66ffd536caade
jsymondst <jammie@Jammie-XPS.localdomain>
2020-05-06 17:51:57 +0100

    added ability for computer to play

--- app/game.rb
+++ app/game.rb
@@ -30,8 +30,8 @@ class Game
         @ai_stack = stack[(stack.length/2) .. stack.length]
 
         # import the categories
-        headers = Card.headers
-        table_headers = Card.table_headers
+        self.headers = Card.headers
+        self.table_headers = Card.table_headers
 
         #tell the user what's happened
         PROMPT.keypress("Dealt each player #{@player_stack.length} cards.", timeout: 3)
@@ -39,11 +39,10 @@ class Game
     end
 
     def player_clash
-        #The main game action.
-        
-        system("clear")
+        #The player's turn
         
         #Game Status
+        system("clear")
         puts "Your Points: #{player_points} | Opponent's Points: #{ai_points}"
         puts "Your card:"
 
@@ -70,15 +69,15 @@ class Game
             input = STDIN.gets.chomp
             selected = options.include?(input)
         end
-        input = input.to_i
+        selection = input.to_i
         
         #Show selected stats        
         
-        player_value = player_card_values[input].to_f
-        ai_value = ai_card_values[input].to_f
+        player_value = player_card_values[selection].to_f
+        ai_value = ai_card_values[selection].to_f
         
-        player_results = ["Your Card:", player_card.name + "  ", headers[input]+":", player_value]
-        ai_results = ["Their Card:", ai_card.name+ "  ", headers[input]+":", ai_value]
+        player_results = ["Your Card:", player_card.name + "  ", headers[selection]+":", player_value]
+        ai_results = ["Their Card:", ai_card.name+ "  ", headers[selection]+":", ai_value]
         
         result_table = TTY::Table.new(player_results, [ai_results])
         puts result_table.render
@@ -86,19 +85,117 @@ class Game
         #Compare stats
 
         if player_value > ai_value
+            clash_result = "player"
             puts "You won!"
             self.player_points += 1
         elsif player_value < ai_value
+            clash_result = "ai"
             puts "You Lost."
             self.ai_points += 1
         else
+            clash_result = "draw"
             puts "It was a Draw!"
         end
 
-        #Show scores before we go again.
+        clash_result
 
-        PROMPT.keypress("Your Points: #{player_points} | Opponent's Points: #{ai_points}", timeout: 3)
+    end
 
+    def ai_clash
+        #The Computer's Turn
+               
+        #Game Status
+        system("clear")
+        puts "Your Points: #{player_points} | Opponent's Points: #{ai_points}"
+        puts "Your card:"
+
+        #grab the first card from each player's stack and import the values
+        
+        player_card = self.player_stack.shift
+        ai_card = self.ai_stack.shift
+        player_card_values = player_card.card_values
+        ai_card_values = ai_card.card_values
+
+        #Import card values into a table and render it
+        
+        card_table = TTY::Table.new table_headers, [player_card_values]        
+        puts card_table.render(:ascii)
+
+        #Tell me the computer is choosing
+
+        puts "It's their turn."
+        PROMPT.keypress("Thinking...", timeout: 3)
+        #The computer chooses a stat (randomly)
+
+        selection = rand(1..deck.playable_columns)
+
+        player_value = player_card_values[selection].to_f
+        ai_value = ai_card_values[selection].to_f
+        
+        player_results = ["Your Card:", player_card.name + "  ", headers[selection]+":", player_value]
+        ai_results = ["Opponent's Card:", ai_card.name+ "  ", headers[selection]+":", ai_value]
+        
+        result_table = TTY::Table.new(player_results, [ai_results])
+        puts result_table.render
+
+        #Compare stats
+
+        if player_value > ai_value
+            clash_result = "player"
+            puts "You won!"
+            self.player_points += 1
+        elsif player_value < ai_value
+            clash_result = "ai"
+            puts "You Lost."
+            self.ai_points += 1
+        else
+            clash_result = "draw"
+            puts "It was a Draw!"
+        end
+        
+        clash_result
+
+    end
+
+    def game_loop
+        #flip a coin to see who goes first
+        active_player = ["ai","player"][rand(0..1)]
+
+        case active_player
+            when "player"
+                flip_message = "You won the coin flip! You're going first."
+            when "ai"
+                flip_message = "Opponent won the coin flip. They will choose first."
+        end
+
+        PROMPT.keypress(flip_message, timeout: 2)
+
+        game_result = nil
+
+        until game_result do
+            case active_player
+                when "ai"
+                    clash_result = ai_clash
+                when "player"
+                    clash_result = player_clash
+            end
+
+            PROMPT.keypress("Your Points: #{player_points} | Opponent's Points: #{ai_points}", timeout: 3)
+            
+            if clash_result != "draw"
+                active_player = clash_result
+            end
+            
+            if player_points == 5
+                game_result = "win"
+            elsif ai_points == 5
+                game_result = "loss"
+            end
+        
+        end
+
+        game_result
+        
     end
         
 
@@ -115,16 +212,7 @@ class Game
 
         #Start playing
 
-        result = nil
-
-        while !result do
-            self.player_clash
-            if player_points == 5
-                result = "win"
-            elsif ai_points == 5
-                result = "loss"
-            end
-        end
+        result = game_loop
 
         #Game complete...
 
